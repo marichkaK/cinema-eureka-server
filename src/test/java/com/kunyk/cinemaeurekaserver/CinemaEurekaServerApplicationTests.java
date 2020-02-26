4e150fa42a838969e2fe9f0517275857868860f4
marichkaK <maria199881@gmail.com>
2020-02-26 10:16:15 +0200

    initial commit

---
+++ src/test/java/com/kunyk/cinemaeurekaserver/CinemaEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunyk.cinemaeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
